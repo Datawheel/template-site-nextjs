1126c1fc1b65b3d7673e88f359b26d7ffc3d84f0
scespinoza <secancinoe@gmail.com>
2023-02-08 12:21:41 -0300

    add mantine dependencies

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
-/* eslint-disable no-undef */
+import {
+  Title, Text, Container, List,
+} from "@mantine/core";
 
 export default function Home() {
   return (
-    <h1>Home</h1>
+    <Container>
+      <Title>NextJS 13 Template</Title>
+      <Text component="p">This is a NextJs 13 template for Datawheel projects</Text>
+      <Text component="p">It includes</Text>
+      <List>
+        <List.Item>Eslint config and github workflows</List.Item>
+        <List.Item>Mantine components</List.Item>
+        <List.Item>Dockefile for deployments</List.Item>
+      </List>
+    </Container>
   );
 }
