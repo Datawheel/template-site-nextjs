aa54a3f832d48aef0891d9ac52dc6f2cc3d44182
scespinoza <secancinoe@gmail.com>
2023-02-09 16:55:26 -0300

    adds Mantine config for Next

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,22 @@
-export default function App({Component, pageProps}) {
-  return <Component {...pageProps} />;
+import {MantineProvider} from "@mantine/core";
+import {DefaultSeo} from "next-seo";
+
+export default function App(props) {
+  const {Component, pageProps} = props;
+
+  return (
+    <>
+      <DefaultSeo title="NextJS Template" titleTemplate="%s | NextJS Template" />
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: "dark",
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  );
 }
