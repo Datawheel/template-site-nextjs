3680647fe6ce71816212df988e2824e5d09f7c40
scespinoza <secancinoe@gmail.com>
2024-03-14 12:35:10 -0300

    adds pathSegment to getServerSideProps

--- pages/cms/[...bespoke].tsx
+++ pages/cms/[...bespoke].tsx
@@ -1,7 +1,10 @@
 import {BespokeManager} from "@datawheel/bespoke";
 import {BespokeManagerServerSideProps} from "@datawheel/bespoke/server";
 
-export const getServerSideProps = BespokeManagerServerSideProps();
+// needs to match the path defined in the NextJS dynamic route
+const pathSegment = "bespoke";
+
+export const getServerSideProps = BespokeManagerServerSideProps({pathSegmentsKey: pathSegment});
 
 export default BespokeManager({
   // title to display in the CMS Application
@@ -9,7 +12,7 @@ export default BespokeManager({
   // the basepath you chose for rendering the reports
   profilePrefix: "/report",
   // the path segment defined on this file. Bby default it assumes it is "bespoke"
-  pathSegment: "bespoke",
+  pathSegment,
   // an instance of NotificationProps: https://v6.mantine.dev/others/notifications/?t=props to manage the BespokeNotifications.
   notifications: {
     position: "bottom-right",
